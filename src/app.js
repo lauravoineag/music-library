cab9f8781042b9fa00eb2f3a3974cbb9a304b5ef
Laura Voineag <lauravoineag87@gmail.com>
2021-12-30 12:54:04 +0000

    separating the controllers and the routes

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 const express = require('express');
+const artistRouter = require('./routes/artist');
+
 const app = express();
+
 app.use(express.json());
+app.use('/artist', artistRouter);
 
 //Add a get route to / in your app.js.
 //It should return a 200 status code and a Hello World string.
 //Run your app and send a GET request to localhost:4000 in postman.
 //If you get a Hello World then you know that your app is working.
 
-app.get('/', (request, response) => {
-  console.log('error');
-  response.status(200).send('Hello World');
-});
+// app.get('/', (request, response) => {
+//   console.log('error');
+//   response.status(200).send('Hello World');
+// });
 
 module.exports = app;
---
+++ src/routes/artist.js
@@ -0,0 +1,9 @@
+// src/routes/artist.js
+const express = require('express');
+const artistController = require('../controllers/artist');
+
+const router = express.Router();
+
+router.post('/', artistController.create);
+
+module.exports = router;
