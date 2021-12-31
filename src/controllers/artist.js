3e24b506764683f20f6997b677c02afe56aebec7
Laura Voineag <lauravoineag87@gmail.com>
2021-12-31 10:11:37 +0000

    Passing the tests for Artist -creating tables and columns

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -1,3 +1,44 @@
-exports.create = (req, res) => {
-  res.sendStatus(201);
+const getDb = require('../services/db');
+
+exports.create = async (req, res) => {
+  const db = await getDb();
+  const { name, genre } = req.body;
+
+  try {
+    await db.query(
+      `INSERT INTO Artist (name, genre) VALUES ('${name}', '${genre}')`
+    );
+
+    res.sendStatus(201);
+  } catch (err) {
+    res.sendStatus(500).json(err);
+  }
+
+  db.close();
 };
+
+// the above written in a different way
+
+// const getDb = require('../services/db');
+
+// const createArtistController = async (req, res) => {
+//   const db = await getDb();
+//   const artistName = req.body.name;
+//   const artistGenre = req.body.genre;
+//   try {
+//     console.log(artistName, artistGenre);
+//     await db.query(
+//       `INSERT INTO Artist (name, genre)
+//     VALUES (?, ?)`,
+//       [artistName, artistGenre]
+//     );
+//     res.sendStatus(201);
+//   } catch (err) {
+//     res.sendStatus(500).json(err);
+//   }
+//   db.close();
+// };
+
+// exports.create = (req, res) => {
+//   res.sendStatus(201);
+// };
