32fd09765ea70466251375b7f408b57e6ab53ce5
Laura Voineag <lauravoineag87@gmail.com>
2021-12-31 09:13:11 +0000

    Creating Artist

--- tests/artist-create.test.js
+++ tests/artist-create.test.js
@@ -1,9 +1,39 @@
-// tests/artist-create.js
+// // tests/artist-create.js
+// const { expect } = require('chai');
+// const request = require('supertest');
+// const app = require('../src/app');
+
+// describe('create artist', () => {
+//   describe('/artist', () => {
+//     describe('POST', () => {
+//       it('creates a new artist in the database', async () => {
+//         const res = await request(app).post('/artist').send({
+//           name: 'Tame Impala',
+//           genre: 'rock',
+//         });
+
+//         expect(res.status).to.equal(201);
+//       });
+//     });
+//   });
+// });
+
+//Modify tests to connect to App DB
+
 const { expect } = require('chai');
 const request = require('supertest');
+const getDb = require('../src/services/db');
 const app = require('../src/app');
 
 describe('create artist', () => {
+  let db;
+  beforeEach(async () => (db = await getDb()));
+
+  afterEach(async () => {
+    await db.query('DELETE FROM Artist');
+    await db.close();
+  });
+
   describe('/artist', () => {
     describe('POST', () => {
       it('creates a new artist in the database', async () => {
@@ -13,6 +43,13 @@ describe('create artist', () => {
         });
 
         expect(res.status).to.equal(201);
+
+        const [[artistEntries]] = await db.query(
+          `SELECT * FROM Artist WHERE name = 'Tame Impala'`
+        );
+
+        expect(artistEntries.name).to.equal('Tame Impala');
+        expect(artistEntries.genre).to.equal('rock');
       });
     });
   });
--- utils/create-database.js
+++ utils/create-database.js
@@ -32,6 +32,16 @@ const setUpDatabase = async () => {
 
     // create the database if it doesn't already exist
     await db.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME}`);
+    //We can add additional db.query() to create-database.js to configure our database.
+
+    // Create Artist Table -Adding queries to the create-database.js script. They should run after the database has been created, but before db.close().
+    await db.query(`USE ${DB_NAME}`);
+    await db.query(`CREATE TABLE IF NOT EXISTS Artist (
+      id INT PRIMARY KEY auto_increment,
+      name VARCHAR(25),
+      genre VARCHAR(25)
+    )`);
+
     db.close();
   } catch (err) {
     // if something goes wrong, console.log the error and the current environment variables
